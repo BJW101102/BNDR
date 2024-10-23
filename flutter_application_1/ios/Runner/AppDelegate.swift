8a9bf38f091708763692509b7f9e331444138e4d
SammySchool <143580004+SammySchool@users.noreply.github.com>
2024-10-23 13:05:35 -0500

    map time

--- flutter_application_1/ios/Runner/AppDelegate.swift
+++ flutter_application_1/ios/Runner/AppDelegate.swift
@@ -1,5 +1,6 @@
 import Flutter
 import UIKit
+import GoogleMaps
 
 @main
 @objc class AppDelegate: FlutterAppDelegate {
@@ -8,6 +9,7 @@ import UIKit
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
+    GMSServices.provideAPIKey("AIzaSyBCM8x-TkF2p1jyh4BHp13RBed1dYtVgmw")
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -2,6 +2,8 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/authentication/login.dart';
 import 'package:flutter_application_1/firebase_options.dart';
+import 'package:flutter_application_1/helpers/map.dart';
+
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -20,7 +22,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Login()
+      home: Map()
     );
   }
 }
