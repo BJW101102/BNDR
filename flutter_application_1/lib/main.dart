52bb2dd455436fef01c57e44e338f3b71b2dd43d
SammySchool <143580004+SammySchool@users.noreply.github.com>
2024-10-25 18:45:36 -0500

    Search Bar with autocomplete works, no interaction with results yet just text

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -24,7 +24,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: FriendsPage()
+      home: Planner()
     );
   }
 }
--- flutter_application_1/lib/pages/planner.dart
+++ flutter_application_1/lib/pages/planner.dart
@@ -1,25 +1,66 @@
+import 'dart:convert';
+
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:google_places_flutter/google_places_flutter.dart';
-
+import 'package:google_places_flutter/model/place_type.dart';
+import 'package:google_places_flutter/model/prediction.dart';
+import 'package:uuid/uuid.dart';
+import 'package:http/http.dart' as http;
 
 class Planner extends StatefulWidget {
   const Planner({super.key});
 
   @override
-  State<Planner> createState() => _GoogleMapFlutterState();
+  State<Planner> createState() => _PlannerState();
 }
 
-class _GoogleMapFlutterState extends State<Planner> {
+class _PlannerState extends State<Planner> {
   LatLng? myCurrentLocation;
   late GoogleMapController googleMapController;
   Set<Marker> markers = {};
+  final searchController = TextEditingController();
+  var uuid = const Uuid();
+  List<dynamic> listOfLocations = [];
+  final String token = '1234567890';
 
   @override
   void initState() {
-    super.initState();
     _getUserLocation();
+    searchController.addListener(() {
+      _onChange();
+    });
+    super.initState();
+  }
+
+  _onChange() {
+    placeSuggestion(searchController.text);
+  }
+
+  void placeSuggestion(String input) async {
+    const String apiKey = "AIzaSyD_eIXoIx5zyyzehtsKDcjiaAyjaZm5A0A";
+    try {
+      String bassedUrl =
+          "https://maps.googleapis.com/maps/api/place/autocomplete/json";
+      String request =
+          '$bassedUrl?input=$input&key=$apiKey&sessiontoken=$token';
+      var response = await http.get(Uri.parse(request));
+      var data = json.decode(response.body);
+      if (kDebugMode) {
+        print(data);
+      }
+      if (response.statusCode == 200) {
+        setState(() {
+          listOfLocations = json.decode(response.body)['predictions'];
+        });
+      } else {
+        throw Exception("Failed to load suggestions");
+      }
+    } catch (e) {
+      print(e.toString());
+    }
   }
 
   void _getUserLocation() async {
@@ -54,50 +95,65 @@ class _GoogleMapFlutterState extends State<Planner> {
       double screenHeight = MediaQuery.of(context).size.height;
 
       return Scaffold(
-        body: Column(
-          children: [
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: GooglePlaceAutoCompleteTextField(
-                googleAPIKey: 'AIzaSyD_eIXoIx5zyyzehtsKDcjiaAyjaZm5A0A',
-                textEditingController: TextEditingController(),
-                inputDecoration: InputDecoration(
-                  hintText: 'Search here',
-                  prefixIcon: Icon(Icons.search),
-                  border: OutlineInputBorder(),
-                ),
-              ),
-            ),
-            
-
-            // Planner in the top half
-            SizedBox(
-              height: screenHeight / 2,
-              child: GoogleMap(
-                myLocationButtonEnabled: false,
-                markers: markers,
-                onMapCreated: (GoogleMapController controller) {
-                  googleMapController = controller;
+        body: Container(
+          margin: EdgeInsets.only(top: 60),
+          child: Column(
+            children: [
+              TextField(
+                controller: searchController,
+                decoration: InputDecoration(hintText: "Search Places..."),
+                onChanged: (value) {
+                  setState(() {});
                 },
-                initialCameraPosition: CameraPosition(
-                  target: myCurrentLocation!,
-                  zoom: 14,
-                ),
               ),
-            ),
-            // Bottom half content
-            Expanded(
-              child: Container(
-                color: Colors.grey[200],
-                child: Center(
-                  child: Text(
-                    "Bottom half of the screen",
-                    style: TextStyle(fontSize: 24),
+              Visibility(
+                visible: searchController.text.isEmpty ? false : true,
+                child: Expanded(
+                  child: ListView.builder(
+                    shrinkWrap: true,
+                    physics: NeverScrollableScrollPhysics(),
+                    itemCount: 5,
+                    itemBuilder: (context, index) {
+                      return GestureDetector(
+                        onTap: () {},
+                        child: Text(
+                          listOfLocations[index]["description"],
+                        ),
+                      );
+                    },
                   ),
                 ),
               ),
-            ),
-          ],
+
+              // Planner in the top half
+              SizedBox(
+                height: screenHeight / 2,
+                child: GoogleMap(
+                  myLocationButtonEnabled: false,
+                  markers: markers,
+                  onMapCreated: (GoogleMapController controller) {
+                    googleMapController = controller;
+                  },
+                  initialCameraPosition: CameraPosition(
+                    target: myCurrentLocation!,
+                    zoom: 14,
+                  ),
+                ),
+              ),
+              // Bottom half content
+              Expanded(
+                child: Container(
+                  color: Colors.grey[200],
+                  child: Center(
+                    child: Text(
+                      "Bottom half of the screen",
+                      style: TextStyle(fontSize: 24),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
         floatingActionButton: FloatingActionButton(
           backgroundColor: Colors.white,
